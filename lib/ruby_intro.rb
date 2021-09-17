19c2ccc915184ce58358dbab3873806b516590d5
EC2 Default User <ec2-user@ip-172-31-41-129.us-east-2.compute.internal>
2021-09-17 04:08:10 +0000

    Assignment done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,101 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+    if arr.length == 0 
+        return 0
+    else 
+        p = arr.inject(0){|sum,x| sum + x }
+        return p
+    end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    if arr.length == 0 
+        return 0
+    elsif arr.length == 1
+        return arr[0]
+    else 
+        q = arr.sort.last(2).sum
+        return q
+    end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+
+    if arr.length == 0
+        return false
+    elsif arr.length == 1
+        return false
+    else 
+        for i in 0...arr.length
+            for j in i+1...arr.length
+                if arr[i]+arr[j] == n
+                    return true
+                end
+            end
+        end
+    end
+return false
+ 
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    if s.length == 0
+        return false
+    end
+    if s[0].match("[a-zA-Z]") 
+        if ['a', 'e', 'i', 'o', 'u', 'A', 'E','I','O','U'].include?s[0]
+            return false
+        else 
+            return true
+        end
+    else 
+        return false
+    end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if s.length == 0
+        return false
+    end
+    if s.chars.all? {|x| x =~ /[01]/}
+        if (s.to_i)%4 == 0
+            return true
+        else 
+            return false
+        end
+    else 
+        return false 
+    end
+  
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    def initialize(isbn,price)
+        unless price.is_a?(Numeric) and price > 0
+            raise ArgumentError.new("Numbers only for price")
+        end
+        unless isbn != ""
+            raise ArgumentError.new("Not valid isbn")
+        end
+        @isbn = isbn
+        @price = price
+    end
+ 
+    attr_accessor :isbn
+    attr_accessor :price
+ 
+ 
+ 
+    def price_as_string
+        return "$%.2f" % @price
+    end
 end
